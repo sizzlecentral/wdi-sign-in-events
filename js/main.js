1cce36a92cd84353dad6876ef88dda4122c86b63
Don Cooper <don_cooper@hotmail.com>
2017-07-06 14:53:07 -0400

    Modal window closing when close button pressed

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   var signIn = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
+  var close = document.querySelector('.close')
 
   signIn.addEventListener('click', function(event) {
     modal.style.display = 'block';
   });
 
+  close.addEventListener('click', function(event) {
+    modal.style.display = 'none';
+  });
 
 });
