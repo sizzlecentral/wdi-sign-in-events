4b8d82700dbb790b6c59ab048fac11273f931286
Don Cooper <don_cooper@hotmail.com>
2017-07-06 15:01:36 -0400

    Errors working for input fields when submit button pressed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   var signIn = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
-  var close = document.querySelector('.close')
+  var close = document.querySelector('.close');
+  var submit = document.querySelector('.submit');
+  var input = document.querySelectorAll('input');
 
   signIn.addEventListener('click', function(event) {
     modal.style.display = 'block';
@@ -10,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   close.addEventListener('click', function(event) {
     modal.style.display = 'none';
+  });
+
+  submit.addEventListener('click', function(event) {
+    for (var i = 0; i < input.length; i++) {
+      input[i].className = 'error';
+    }
   });
 
 });
